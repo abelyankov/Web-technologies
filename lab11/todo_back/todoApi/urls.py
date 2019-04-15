78bd1b3262f13cb59520019b7ff899cdb7591d98
Arthur <arthurbelyankove@gmail.com>
2019-04-15 20:54:08 +0600

    json presenter & routing

--- lab11/todo_back/todoApi/urls.py
+++ lab11/todo_back/todoApi/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url('', views.index, name='index'),
+    path('task_lists/', views.list),
+    path('task_lists/<int:pk>/', views.list_id),
+    path('task_lists/<int:pk>/tasks/', views.task),
+    path('tasks/<int:pk>', views.task_detail)
 ]
