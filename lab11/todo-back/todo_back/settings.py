b7d35ba5caea02a70ac0165335fdfb29c710d302
Arthur <arthurbelyankove@gmail.com>
2019-04-17 16:10:03 +0600

    Get TaskList with todo-front

--- lab11/todo-back/todo_back/settings.py
+++ lab11/todo-back/todo_back/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'todoApi',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +42,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
---
+++ lab11/todo-front/src/app/task-list.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+// import { ITask } from './itask';
+import {ITaskList} from './itask-list';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subject, asapScheduler, pipe, of, from, interval, merge, fromEvent } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskListService {
+
+  constructor(private http: HttpClient) { }
+  taskListURL = 'http://127.0.0.1:8000/task_lists'
+
+  getTaskLists(): Promise<ITaskList[]> {
+    return this.http.get(this.taskListURL).toPromise().then(
+      response => response as ITaskList[]
+      );
+  }
+}
