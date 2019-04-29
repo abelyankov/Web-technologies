974a6a1ede5333ee76951d57a9f4aabb31042c8d
Arthur <arthurbelyankove@gmail.com>
2019-04-30 00:05:20 +0600

    QUIZ: preparation done

--- quiz/quiz-front/src/app/app.module.ts
+++ quiz/quiz-front/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {NgModule, Provider} from '@angular/core';
+import { ClassProvider, NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import {AuthInterceptor} from './AuthInterceptor';
+import {ProviderService} from './provider.service';
 
 
 @NgModule({
@@ -17,7 +19,14 @@ import {FormsModule} from '@angular/forms';
     HttpClientModule,
     FormsModule
   ],
-  providers: [ Provider ],
+  providers: [
+    ProviderService,
+    <ClassProvider> {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
