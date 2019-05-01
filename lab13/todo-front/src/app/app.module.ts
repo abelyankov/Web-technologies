8e66b643f967e4ee5ab0a11269e03df18e520cb5
Arthur <arthurbelyankove@gmail.com>
2019-05-01 15:57:57 +0600

    copy lab12 for lab13

---
+++ lab13/todo-front/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { TaskListService } from './task-list.service';
+import { TaskListDetailComponent } from './task-list-detail/task-list-detail.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+import {FormsModule} from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponent,
+    TaskListDetailComponent,
+    TaskDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [ TaskListService ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
